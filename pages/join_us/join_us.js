f7b94628d98ab9f97cf54b011c7035c13bda26a7
SarahL726 <SarahL726@github.com>
2022-01-13 03:29:36 -0500

    update join_us

--- pages/join_us/join_us.js
+++ pages/join_us/join_us.js
@@ -1,13 +1,3 @@
-
 Page({
 
-  data: {
-    staffA: {firstName: 'Item1', lastName: 'account1', img:'join_us/主席团合照.png'},
-    staffB: {firstName: 'Item2', lastName: 'account2', img:'join_us/主席团合照.png'},
-    staffC: {firstName: 'Item3', lastName: 'account3', img:'join_us/主席团合照.png'}
-  }
-
-})
-
-Page({});
-
+});
