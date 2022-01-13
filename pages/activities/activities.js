d0f19db8eb9bfc9d705a82f60169f864279a1aa9
SarahL726 <SarahL726@github.com>
2022-01-13 02:10:03 -0500

    update activities

--- pages/activities/activities.js
+++ pages/activities/activities.js
@@ -5,18 +5,28 @@ Page({
     })
   },
   trailer2(){
-    // url: 'pages/activities/Trailers/trailer2'
+    wx.navigateTo({
+      url: '/pages/activities/Trailers/trailer2',
+    })
   },
   trailer3(){
-    // url: 'pages/activities/Trailers/trailer3'
+    wx.navigateTo({
+      url: '/pages/activities/Trailers/trailer3',
+    })
   },
   passed1(){
-    // url: 'pages/activities/Passed/passed1'
+    wx.navigateTo({
+      url: '/pages/activities/Passed/passed1',
+    })
   },
   passed2(){
-    // url: 'pages/activities/Passed/passed2'
+    wx.navigateTo({
+      url: '/pages/activities/Passed/passed2',
+    })
   },
   passed3(){
-    // url: 'pages/activities/Passed/passed3'
+    wx.navigateTo({
+      url: '/pages/activities/Passed/passed3',
+    })
   },
 });
