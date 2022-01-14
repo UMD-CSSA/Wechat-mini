3285a610af61911afc73f69abd826a6863c81c11
SarahL726 <SarahL726@github.com>
2022-01-14 00:46:36 -0500

    update home

--- pages/home/home.js
+++ pages/home/home.js
@@ -23,28 +23,6 @@ Page({
     })
   },
 
-  // data: {
-  //   slider: [
-  //     {'img':'/images/news1.jpeg'},
-  //     {'img':'/images/news2.jpeg'},
-  //     {'img':'/images/news3.png'},
-  //     {'img':'/images/news4.jpeg'},
-  //     {'img':'/images/news5.jpeg'}
-  //   ],
-  //   swiperCurrent: 0,
-  // },
-
-  // swiperChange: function(e){
-  //   this.setData({
-  //     swiperCurrent: e.detail.current
-  //   })
-  // },
-
-  data: {
-    selected: true,
-    selected1: false,
-    selected2:false,
-  },
   selected: function (e) {
     this.setData({
       selected: true,
@@ -93,64 +71,11 @@ Page({
     })
   },
 
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-  
-  },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-  
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-  
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-  
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-  
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-  
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-  
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-  
-  },
-
   data: {
+    selected: true,
+    selected1: false,
+    selected2:false,
+    
     swiperCurrent: 0,//当前所在页面的 index
     indicatorDots: true, //是否显示面板指示点
     autoplay: true, //是否自动切换
@@ -166,11 +91,11 @@ Page({
     ],
 
     links: [//点击图片之后跳转的路径
-      'https://mp.weixin.qq.com/s?__biz=Mzg5NjY2ODg3MQ==&mid=2247497678&idx=1&sn=1064da429dec4e27da3d5659c3a62b77&chksm=c07f3266f708bb70d5bf87fbdf76e6d73575ff79e088cf3df09a183d7d8ffdeaffa5ec6c22ed&token=1794852101&lang=zh_CN#rd',
-      '../personal/personal',
-      '../personal/personal',
-      '../personal/personal',
-      '../personal/personal',
+      '/pages/home/news/news1',
+      '/pages/home/news/news2',
+      '/pages/home/news/news3',
+      '/pages/home/news/news4',
+      '/pages/home/news/news5'
     ] 
   },
 
@@ -191,7 +116,7 @@ Page({
   //点击图片触发事件
   swipclick: function (e) {
     console.log(this.data.swiperCurrent);
-    wx.switchTab({
+    wx.navigateTo({
       url: this.data.links[this.data.swiperCurrent]
     })
   },
