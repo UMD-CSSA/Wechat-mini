313d83a12b924a801cb4fbba5547a9c682406c9e
SarahL726 <SarahL726@github.com>
2022-01-13 21:41:18 -0500

    update home

--- pages/home/home.js
+++ pages/home/home.js
@@ -40,18 +40,85 @@ Page({
   },
 
   data: {
-    type: 'policy',
+    selected: true,
+    selected1: false,
+    selected2:false,
   },
-
-  onLoad: function(options) {},
-
-  handleType(event) {
-    const type = event.currentTarget.dataset.type;
+  selected: function (e) {
     this.setData({
-      type: type,
-    });
+      selected: true,
+      selected1: false,
+      selected2: false
+    })
   },
+  selected1: function (e) {
+    this.setData({
+      selected: false,
+      selected1: true,
+      selected2: false
+    })
+  },
+  selected2: function (e) {
+    this.setData({
+      selected: false,
+      selected1: false,
+      selected2: true
+    })
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+  
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+  
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+  
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+  
+  }
 })
-
-
-
